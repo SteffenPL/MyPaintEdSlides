cd3fa698304a8ddfde2683f1e3b7b235d75eb26a
SteffenPL <steffen.plunder@web.de>
2018-04-19 21:15:31 +1200

    updated slides with a full presentation, given at University of Auckland. Some mistakes found during the talk are hopefully fixed now.

--- test/test_ora_presentation.py
+++ test/test_ora_presentation.py
@@ -9,7 +9,7 @@ from ora_reader import OraPresentation, OraReader
 #ora.load_file("test_slide.ora")
 #ora.generate_slides(with_background=False, overwrite=False)
 
-p = OraPresentation(buildDir="/tmp/build", outputDir="output_2")
+p = OraPresentation(buildDir="/tmp/build", outputDir="output_complete")
 p.load_from_folder("test_complete/*.ora",with_background=False,overwrite=True)
 
 p.write_html(html_template_filename="../html_templates/default.html_template")
